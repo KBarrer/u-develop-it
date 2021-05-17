d049a299f20c117a5dedc293637fbd073eb70658
KSB <kyle.barrer@gmail.com>
2021-05-17 10:51:19 -0600

    added SQL query and callback

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 
@@ -8,9 +9,26 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+       host: 'localhost',
+       //your MySQL username
+       user: 'root',
+       // your MySQL password
+       password: 'B@rrer@rt00',
+       database: 'election' 
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 
 // Default response for any other request (Not Found)
-app.use ((req, res) => {
+app.use((req, res) => {
     res.status(404).end();
 });
 
